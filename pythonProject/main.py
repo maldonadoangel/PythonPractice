68a7ce71a6a2e80793f2b5b33262835bb8ea6637
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-04 21:23:44 -0600

    Tipo Booleanos

---
+++ pythonProject/main.py
@@ -0,0 +1,13 @@
+#Tipos booleanos
+
+miVariable = False
+print(miVariable)
+
+miVariable = 3 > 1
+print(type(miVariable))
+print(miVariable)
+
+if miVariable:
+    print("El resultado fue verdadero")
+else:
+    print("El resultado es Falso")
