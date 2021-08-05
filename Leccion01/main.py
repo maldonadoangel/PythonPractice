8e6ec032eea5c22a1fb7b55f5c181c4a4cfc1d61
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-04 21:17:58 -0600

    Variables en Python

---
+++ Leccion01/main.py
@@ -0,0 +1,36 @@
+# Tipo int
+x = 10
+y = 2
+print(x)
+print(id(x))
+print(type(x))
+# flotante
+x = 14.5
+print(x)
+print(id(x))
+print(type(x))
+# string
+x = 'Soy un string'
+print(x)
+print(id(x))
+print(type(x))
+# tipo boolean
+x = True
+print(x)
+print(id(x))
+print(type(x))
+
+# cadena (string)
+miGrupoFavorito = 'AeroSmith'
+comentario = 'The Best Rock Band'
+print("Mi grupo favorito es: ", miGrupoFavorito, comentario)
+
+numero1 = "10"
+numero2 = "3"
+# Nos devuelve la suma y convertimos los string en entero
+print("Concatenacion:", int(numero1) + int(numero2))
+
+# Ejemplo de solo enteros
+numero1 = 10
+numero2 = 3
+print("Concatenacion:", numero1 + numero2)
