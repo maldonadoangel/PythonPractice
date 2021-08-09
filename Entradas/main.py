cf9840ff400f860d85c5b21191f16d1db4773319
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-09 14:47:00 -0600

    Conversion de entradas en python

---
+++ Entradas/main.py
@@ -0,0 +1,6 @@
+#Funcion input para procesar la entrada de los usuarios
+numero = int(input("Escribe un numero: "))
+numero2 = int(input("Escribe otro numero: "))
+
+resultado = numero + numero2
+print("El resultado de la suma es: ", resultado)
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -2,12 +2,15 @@
 
 miVariable = False
 print(miVariable)
-
+miNombre = 'Angel Morales'
+edad = 23
 miVariable = 3 > 1
 print(type(miVariable))
 print(miVariable)
 
-if miVariable:
+if 1 > 3:
     print("El resultado fue verdadero")
 else:
     print("El resultado es Falso")
+
+print(miNombre, edad)
