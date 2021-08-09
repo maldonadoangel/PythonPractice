60252f0465c7842faa8c2d340dfd0dc8d9cdc802
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-09 15:13:43 -0600

    Ejercicio califica tu dia

---
+++ CalificaTuDia1/main.py
@@ -0,0 +1,4 @@
+#Ejercicio 1, califica tu dia del 1 al 10
+print('Bienvenido al programa para calificar tu dia')
+calificacion = int(input('Del 1 al 10 que tal estuvo tu dia: '))
+print("Tu dia estuvo un:", calificacion)
