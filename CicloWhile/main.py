85e87de17c7cca0343f8dec0190a787563290e23
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-12 02:32:24 -0600

    ciclo while

---
+++ CicloWhile/main.py
@@ -0,0 +1,14 @@
+""" condicion = False
+
+while condicion:
+    print('Ejecutando ciclo while')
+else:
+    print('Fin del ciclo while') """
+
+contador = 0
+
+while contador < 3:
+    print(f'Tu contador lleva el valor: {contador}')
+    contador += 1
+else:
+    print('Fin del ciclo while')
