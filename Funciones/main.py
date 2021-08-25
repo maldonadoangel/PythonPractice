3b7ee796d473e02d17cd27da108544fb4ef687e3
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-25 11:35:10 -0600

    return en funciones

--- Funciones/main.py
+++ Funciones/main.py
@@ -1,17 +1,6 @@
+def sumar(a,b):
+    return a+b
 
-#parametros se pasan en el () de nuestra funcion a traves de los argumentos
-def mi_funcion(nombre, apellido):
-    print(f'Hola {nombre} {apellido}, estoy Saludando desde mi funcion')
+total = sumar(5,5)
 
-
-#los argumentos son los valores que pasamos nosotros
-mi_funcion('Angel', 'Morales')
-
-mi_funcion('Karla', 'Lara')
-#Solicitando valores como argumentos para pasarlo como paramentros en la funcion
-name = input('Ingresa tu nombre: ')
-last_name = input('Ingresa tu apellido: ')
-
-resultado = mi_funcion(name, last_name)
-
-print(resultado)
+print(f'La suma de nuestra funcion es: {total}')
