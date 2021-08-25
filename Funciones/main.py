3380133ad004c9f318c15e2d1adc3c1644569b87
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-25 11:26:39 -0600

    Pasando parametros y argumentos

--- Funciones/main.py
+++ Funciones/main.py
@@ -1,6 +1,10 @@
-def mi_funcion():
-    print('Saludando desde mi funcion')
 
-mi_funcion()
-mi_funcion()    
+#parametros se pasan en el () de nuestra funcion a traves de los argumentos
+def mi_funcion(nombre, apellido):
+    print(f'Hola {nombre} {apellido}, estoy Saludando desde mi funcion')
+
+
+#los argumentos son los valores que pasamos nosotros
+mi_funcion('Angel', 'Morales')
+
     
