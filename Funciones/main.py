0e299769e68f6666e3ad123d8821bfc520d855a0
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-26 10:36:14 -0600

    Parametros por defecto, parametros variables

--- Funciones/main.py
+++ Funciones/main.py
@@ -4,3 +4,24 @@ def sumar(a,b):
 total = sumar(5,5)
 
 print(f'La suma de nuestra funcion es: {total}')    
+
+
+
+#Parametros por defecto, nos ayuda a no dejar sin rellenar algun parametro
+def resta(a = 0, b = 0):
+    return a-b
+
+
+
+sustraccion = resta()
+print(f'La resta de la funcion es: {sustraccion}')
+print()
+
+#parametros variables en una funcion, cuando desconocemos la cantidad de parametros que van a ingresar
+def lista_nombres(*nombres):
+    for nombre in nombres:
+        print(f'nombre: {nombre}    ')
+
+lista = lista_nombres('Angel', 'Hernan', 'Jose', 'Herni')
+
+print(lista)
