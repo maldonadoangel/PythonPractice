be7851b7e6a359ef3941e4cdf4480ae9031ba238
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-30 04:17:10 -0500

    Crear mas de un objeto

--- clases_y_objetos/main.py
+++ clases_y_objetos/main.py
@@ -13,4 +13,14 @@ print(persona2.nombre)
 print(persona2.apellido)
 #Imprimimos la edad a traves de los parametros y el fstring
 print(f'Tu edad es de {persona2.edad} años')
-        
+
+
+#Segundo objeto creado 
+persona2 = Persona('Karlan', 'Martinez', 40)
+print(persona2.nombre)
+print(persona2.apellido)
+print(persona2.edad)
+print('------------------------------------------')
+#Tercer objeto creado en una sola linea de texto
+persona3 = Persona('Juan', 'Castillo', 26)
+print(f'Tu nombre es: {persona3.nombre}, tu apellido es: {persona3.apellido} y tu edad es de {persona3.edad} años')
