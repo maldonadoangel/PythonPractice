3f5e9fe0286797fb57bce1d0ea8b4331a0488625
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-30 04:25:10 -0500

    Modificamos los objetos, despues de que fueron creados

--- clases_y_objetos/main.py
+++ clases_y_objetos/main.py
@@ -13,14 +13,17 @@ print(persona2.nombre)
 print(persona2.apellido)
 #Imprimimos la edad a traves de los parametros y el fstring
 print(f'Tu edad es de {persona2.edad} años')
-
-
-#Segundo objeto creado 
-persona2 = Persona('Karlan', 'Martinez', 40)
+print()
+print('Modificamos la persona 2')
+#Modificamos los atributos de nuestras clases
+persona2.nombre = 'Jason'
+persona2.apellido = 'Mencoz'
+persona2.edad = 30
 print(persona2.nombre)
 print(persona2.apellido)
-print(persona2.edad)
-print('------------------------------------------')
+print(f'Tu edad es de {persona2.edad} años')
+print()
+
 #Tercer objeto creado en una sola linea de texto
 persona3 = Persona('Juan', 'Castillo', 26)
 print(f'Tu nombre es: {persona3.nombre}, tu apellido es: {persona3.apellido} y tu edad es de {persona3.edad} años')
