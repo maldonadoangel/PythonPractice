6c1b13f89e5ac9175749d4c0092d5e3905de92fd
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-30 17:33:29 -0500

    Metodos o instancias de nuestra clase

--- clases_y_objetos/main.py
+++ clases_y_objetos/main.py
@@ -3,11 +3,18 @@ class Persona():
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
+    
+#Creamos metodos para nuestra clase persona
+    def mostrarDetalle(self):
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
+        
 
-persona1 = Persona()
-print(persona1.nombre)
+#Utilizamos el metodo de mostrar detalle para persona1
+persona1 = Persona('Yessica', 'Lopez', 25)
+print(persona1.mostrarDetalle())
 print('------------------')
 print()
+
 persona2 = Persona('Angel Hernan', 'Morales Maldonado', 23)
 print(persona2.nombre)
 print(persona2.apellido)
@@ -15,6 +22,7 @@ print(persona2.apellido)
 print(f'Tu edad es de {persona2.edad} años')
 print()
 print('Modificamos la persona 2')
+
 #Modificamos los atributos de nuestras clases
 persona2.nombre = 'Jason'
 persona2.apellido = 'Mencoz'
