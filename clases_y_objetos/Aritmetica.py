02c996e9f8dd8638464c3a2744055222c3babca2
Angel Morales <maldonadoangel7069@gmail.com>
2021-08-30 18:33:44 -0500

    Ejercicio, clase aritmetica

---
+++ clases_y_objetos/Aritmetica.py
@@ -0,0 +1,34 @@
+class Aritmetica():
+    def __init__(self, numero1 = 0.0, numero2 = 0.0, total = 0.0):
+        self.numero1 = numero1
+        self.numero2 = numero2
+        self.total = total
+    def suma(self):
+        self.total = self.numero1 + self.numero2
+        print(f'La suma de {self.numero1} + {self.numero2} = {self.total}')
+    def resta(self):
+        self.total = self.numero1 - self.numero2
+        print(f'La resta de {self.numero1} - self.numero2 = {self.total}')
+    def multiplicacion(self):
+        self.total = self.numero1 * self.numero2
+        print(f'La multiplicacion de {self.numero1} * {self.numero2} = {self.total}')
+    def division(self):
+        if self.numero2 == 0:
+            print('Error la division no puede ser efectuada, no puedes tener un cero en el denominador, intentalo de nuevo')
+        else:
+            self.total = self.numero1 / self.numero2
+            print(f'La division de {self.numero1} / {self.numero2} = {self.total}')
+
+aritmetica = Aritmetica(10, 50)
+aritmetica.suma()
+aritmetica.resta()
+aritmetica.multiplicacion()
+aritmetica.division()
+print()
+print('------------------------------')
+
+aritmetica2 = Aritmetica(30, 1)
+aritmetica2.suma()
+aritmetica2.resta()
+aritmetica2.multiplicacion()
+aritmetica2.division()
