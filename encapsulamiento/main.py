f5f076a92e2fae8024b3dc1fe6020ad2ad7f6f02
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-01 05:51:53 -0600

    propiedad get y set

--- encapsulamiento/main.py
+++ encapsulamiento/main.py
@@ -4,14 +4,25 @@ class Persona():
         self.apellido = apellido
         self.edad = edad
         self.telefono = telefono
-
+    
+    #Decorador, modifica el comportamiento de nuestro metodo
+    #Esta es la forma de nombrar el metodo getter
+    @property
+    def nombre(self):
+        print('Llamando metodo get nombre')
+        return self._nombre
+    #esta es la forma de nombrar el metodo setter
+    @nombre.setter
+    def nombre(self, nombre):
+        print('llamando metodo set nombre')
+        self._nombre = nombre
+        
     def mostrarPersona(self):
         print(f'Tu nombre es: {self._nombre} y tu apellido es: {self.apellido}, tienes {self.edad} años y tu numero de celular es el {self.telefono}')
         
 persona = Persona('Angel Hernan', 'Morales Maldonado', 23, '50088915')
-#Modificamos el atributo encapsulado
-persona._nombre = 'Hernan Jose'
-persona.mostrarPersona()
-persona.dpi = '2988166440101'
-print(persona.dpi)
-        
+#Gracias al decorador ya no es necesario colocar parentesis en nombre
+#debido a que cambia su comportamiento y es similar al llamar un atributo
+print(persona.nombre)
+persona.nombre = "Juancho"
+print(persona.nombre)
