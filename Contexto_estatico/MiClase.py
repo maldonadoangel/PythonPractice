facc6646c4998fe541510e3bb250d279daa7a642
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-06 20:47:20 -0500

    metodo Estatico

--- Contexto_estatico/MiClase.py
+++ Contexto_estatico/MiClase.py
@@ -5,7 +5,16 @@ class MiClase():
     
     def __init__(self, variable_instancia):
         self.variable_instancia = variable_instancia
+    #no utilizamos el metodo self en nuestro metodo estatico, debido a que no son posibles instanciarlo con las funciones de clase que estan relacionadas a los objetos
+    @staticmethod
+    def metodo_estatico():
+    #No podemos acceder al meotodo de instancia self por lo tanto no podemos especificar atributos de tipo objeto en las clases estaticas
+    # self.variable_instancia
+    #Tenemos que acceder a los objetos atraves de la clases
+        print(MiClase.variable_clase)
 
-print(MiClase.variable_clase)
-miClase = MiClase('Variable de instancia')
-print(miClase.variable_instancia)
+#print(MiClase.variable_clase)
+#miClase = MiClase('Variable de instancia')
+#print(miClase.variable_instancia)
+
+MiClase.metodo_estatico()
